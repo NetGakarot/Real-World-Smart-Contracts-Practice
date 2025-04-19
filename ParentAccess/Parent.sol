5980a777f8e32b8dabd68b50e0e7fee90b4f8003
Gakarot <startradingcentre@gmail.com>
2025-04-20 02:50:27 +0530

    Update Parent.sol

--- ParentAccess/Parent.sol
+++ ParentAccess/Parent.sol
@@ -1,1 +1,54 @@
 
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+/*16.Parent Contract Accessor
+One contract stores data. Another contract fetches and modifies it via interface.
+Use instance + call + interface.
+*/
+
+contract Parent {
+    address public owner;
+
+    uint a;
+    uint b;
+    uint c;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyBy() {
+        require(msg.sender==owner,"Not Authorized");
+        _;
+    }
+
+    function changeOwner(address newOwner) external onlyBy {
+        require(newOwner != address(0),"Invalid address");
+        owner = newOwner;
+    }
+
+    function setValueA(uint _a) external {
+        a = _a;
+    }
+    
+    function setValueB(uint _b) external {
+        b = _b;
+    }
+
+    function setValueC(uint _c) external {
+        c = _c;
+    }
+
+    function getValueA() external view returns (uint) {
+        return a;
+    }
+
+    function getValueB() external view returns (uint) {
+        return b;
+    }
+
+    function getValueC() external view returns (uint) {
+        return c;
+    }
+}
